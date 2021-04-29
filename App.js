9e5b322317608276c44f79b9a87876dd97e0f344
unknown <bouchraelaili247@gmail.com>
2021-04-29 23:51:02 +0000

    currency

---
+++ App.js
@@ -0,0 +1,9 @@
+import React,{ Component} from "react"
+ import Converter from './currency-converter/converter'
+
+ class App extends Component {
+   render() {
+     return <Converter/>;
+   }
+ }
+ export default App;
